2831921853d22c967e20ec6a0fa8b9fd07c3386a
HinataTomari <canadahinata@gmail.com>
2022-12-16 14:50:31 -0800

    First commit

---
+++ src/DummyData.js
@@ -0,0 +1,22 @@
+export const PostsData = [
+  {
+    id: 1,
+    name: "Hinata",
+    content: "Im studying Redux",
+  },
+  {
+    id: 2,
+    name: "No name",
+    content: "today is so cold",
+  },
+  {
+    id: 3,
+    name: "Teacher",
+    content: "Holiday comminggg!!",
+  },
+  {
+    id: 4,
+    name: "Jojo",
+    content: "Chrismasss!!",
+  },
+];
